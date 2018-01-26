803e0fa1bffe4702ff574d1a62eeab554217b35d
bmillaard <32769428+bmillaard@users.noreply.github.com>
2018-01-26 11:16:23 +0000

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -5,13 +5,20 @@ nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 ## Format date and active power column
 datafile$Date <- as.Date(datafile$Date, format="%d/%m/%Y") 
 datafile$Global_active_power <- as.numeric(datafile$Global_active_power)
- ## Subsetting the data 
+
+## Subsetting the data 
 
 subsetdata <- subset(datafile, Date == "2007-02-01" | Date =="2007-02-02")
 
+## Converting the dates
+
+datetime <- paste(as.Date(subsetdata$Date), subsetdata$Time) 
+subsetdata$datetime <- as.POSIXct(datetime) 
 
 ## Create the plot
 
+plot(subsetdata$Global_active_power ~ subsetdata$datetime, type="l", 
+     ylab="Global Active Power (kilowatts)", xlab="") 
 
 
 ## Saving to file 
