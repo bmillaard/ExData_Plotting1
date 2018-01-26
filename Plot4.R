685b376cd6c180bf185d05dc052764789698a199
bmillaard <32769428+bmillaard@users.noreply.github.com>
2018-01-26 10:54:21 +0000

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,1 +1,19 @@
+## Getting full dataset from working directory 
+datafile <- read.csv("./EDA/household_power_consumption.txt", header=T, sep=';', na.strings="?",  
+nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
 
+## Format date and active power column
+datafile$Date <- as.Date(datafile$Date, format="%d/%m/%Y") 
+datafile$Global_active_power <- as.numeric(datafile$Global_active_power)
+ ## Subsetting the data 
+
+subsetdata <- subset(datafile, Date == "2007-02-01" | Date =="2007-02-02")
+
+
+## Create the plot
+
+
+
+## Saving to file 
+dev.copy(png, file="plot4.png", width=480, height=480) 
+dev.off() 
